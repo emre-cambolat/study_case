3d0ef2834a9c9b80a3438d035c9ef779f598381b
emre-cambolat <cambolatemre@outlook.com>
2022-11-17 23:09:59 +0300

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -31,26 +31,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Study Case"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          // await ApiServices.fetchExercies();
-        },
-      ),
-    );
-  }
-}
